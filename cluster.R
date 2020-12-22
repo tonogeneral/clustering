3844c9579f67d350450df93426b61905bdd09d7d
tonogeneral <tonogeneral@gmail.com>
2020-12-22 01:20:59 -0300

    Se crean cluster jeráquico por distancia euclidiana.
    Se crea un dendograma

--- cluster.R
+++ cluster.R
@@ -1,11 +1,6 @@
 #Gráfico en cluster
 #Creado por Gabriel General
-<<<<<<< HEAD
 #22-12-2020 01:05
-
-=======
-#22-12-2020
->>>>>>> a92b69029af243ce9394cc20fe34904f2774258d
 
 set.seed(1234)
 par(mar = c(0,0,0,0))
@@ -13,3 +8,10 @@ x <-rnorm(12, mean = rep(1:3,each = 4), sd = 0.2)
 y <-rnorm(12, mean = rep(c(1,2,1), each = 4), sd = 0.2)
 plot(x,y, col = "blue",pch = 19, cex =2)
 text(x + 0.05, y + 0.05, labels = as.character(1:12))
+
+
+
+# CLUSTERING JERÁRQUICO POR DISTANCIA EUCLIDIANA.
+dataframe <- data.frame(x = x, y = y )
+#Calcula la distancia entre los puntos de un dataframe
+dist(dataframe)
